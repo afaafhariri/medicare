e89b85d03b12f2db5d1b10a5a77a445420c1049b
Afaaf Hariri <afaafhariri@icloud.com>
2025-10-08 22:08:35 +0530

    print success message

--- server/userservice/src/main/java/com/medicareserver/userservice/UserserviceApplication.java
+++ server/userservice/src/main/java/com/medicareserver/userservice/UserserviceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
+        SpringApplication.run(UserserviceApplication.class, args);
+        System.out.println("Let's go! Application is up and running");
 	}
 
 }
