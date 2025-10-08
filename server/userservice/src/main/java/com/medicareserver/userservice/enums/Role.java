38239fb4d621c26e49f64aea26a19f92472bfcbe
Afaaf Hariri <afaafhariri@icloud.com>
2025-10-08 21:13:03 +0530

    feat: user model

--- server/userservice/src/main/java/com/medicareserver/userservice/enums/Role.java
+++ server/userservice/src/main/java/com/medicareserver/userservice/enums/Role.java
@@ -1,4 +1,7 @@
 package com.medicareserver.userservice.enums;
 
 public enum Role {
+    ADMIN,
+    DOCTOR,
+    PATIENT
 }
--- server/userservice/src/main/java/com/medicareserver/userservice/models/Address.java
+++ server/userservice/src/main/java/com/medicareserver/userservice/models/Address.java
@@ -1,4 +1,21 @@
 package com.medicareserver.userservice.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "adresses")
+@Getter
+@Setter
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String street;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private String province;
 }
